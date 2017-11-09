7d914b486a006c82655b45a1161997dfefa283f7
janggunhee <jan9.9unhee@gmail.com>
2017-11-09 13:56:57 +0900

    PostList에 Mixins를 사용

--- instagram/post/apis.py
+++ instagram/post/apis.py
@@ -1,5 +1,6 @@
 from django.http import Http404
-from rest_framework import status
+from requests import post
+from rest_framework import status, mixins, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -7,30 +8,49 @@ from post.seriallizers import PostSerializer
 from .models import Post
 
 
-class PostList(APIView):
-    # api/post/
-    def get(self, request, format=None):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
+class PostList(mixins.ListModelMixin,
+               mixins.CreateModelMixin,
+               generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    # def get(self, request, format=None):
+    #     posts = Post.objects.all()
+    #     serializer = PostSerializer(posts, many=True)
+    #     return Response(serializer.data)
+    #
+    # # def post()구현 (Post 생성)
+    # # 중간에 request.user를 사용해야함
+    #
+    # def post(self, request):
+    #     serializer = PostSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         # 이 과정에서 author값을 request.user로 채우기
+    #         serializer.save(author=self.request.user)
+    #         return Response(serializer.data,  status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Post.objects.get(pk=pk)
+        except Post.DoesNOtExist:
+            raise
+
+    def get(self, request, pk):
+        Post = self.get_object(pk)
+        serializer = PostSerializer(post, data=request.data)
         return Response(serializer.data)
 
-    # def post()구현 (Post 생성)
-    # 중간에 request.user를 사용해야함
-
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=self.request.user)
-            return Response(serializer.data,  status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class PostDetail(APIView):
-#     def get_object(self, pk):
-#         try:
-#             return Post.objects.get(pk=pk)
-#         except Post.DoesNOtExist:
-#             raise
-#     def get(self, request, pk):
 
 
